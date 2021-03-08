543b2516c878ff46eb3ef27cc9939c8a0a72c5f0
Yoann <yoannpascalbouix@gmail.com>
2021-03-08 18:58:32 +0100

    Delete todos

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -4,6 +4,19 @@ const ListTodos = () => {
 
     const [todos, setTodos] = useState([])
 
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            })
+            
+            setTodos(todos.filter(todo => todo.todo_id !== id))
+
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+
     const getTodos = async () => {
         try {
             const response = await fetch("http://localhost:5000/todos")
@@ -18,7 +31,7 @@ const ListTodos = () => {
         getTodos()
     }, [])
 
-    console.log(todos)
+
     return (
         <Fragment>
             <table className="table mt-5 text-center">
@@ -36,10 +49,12 @@ const ListTodos = () => {
                     <td>john@example.com</td>
                 </tr>*/}
                 {todos.map(todo => (
-                    <tr>
+                    <tr key={todo.todo_id}>
                         <td>{todo.description}</td>
                         <td>Edit</td>
-                        <td>Delete</td>
+                        <td>
+                            <button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button>
+                        </td>
                     </tr>
                 ))}
                 
