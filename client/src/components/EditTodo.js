28b3ffd27bef4ae1fbf411cb36ab1650f786198a
Yoann <yoannpascalbouix@gmail.com>
2021-03-08 19:34:37 +0100

    Cancel edit feature

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -23,13 +23,13 @@ const EditTodo = ({todo}) => {
             <button type="button" className="btn btn-warning" data-toggle="modal" data-target={`#id${todo.todo_id}`}>
             Edit
             </button>
-            <div className="modal" id={`id${todo.todo_id}`}>
+            <div className="modal" id={`id${todo.todo_id}`} onClick={() => setDescription(todo.description)}>
             <div className="modal-dialog">
                 <div className="modal-content">
 
                 <div className="modal-header">
                     <h4 className="modal-title">Edit todo</h4>
-                    <button type="button" className="close" data-dismiss="modal">&times;</button>
+                    <button type="button" className="close" data-dismiss="modal" onClick={() => setDescription(todo.description)}>&times;</button>
                 </div>
 
                 <div className="modal-body">
@@ -40,7 +40,7 @@ const EditTodo = ({todo}) => {
                     <button type="button" className="btn btn-warning" data-dismiss="modal" onClick={e => updateDescription(e)}>Edit</button>
                 </div>
                 <div className="modal-footer">
-                    <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+                    <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={() => setDescription(todo.description)}>Close</button>
                 </div>
 
                 </div>
