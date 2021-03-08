6cd8d70f03471b0465f3ac8dfc06253063207386
Yoann <yoannpascalbouix@gmail.com>
2021-03-08 18:48:26 +0100

    Get todos and list them

---
+++ client/src/components/ListTodos.js
@@ -0,0 +1,52 @@
+import React, { Fragment, useEffect, useState } from 'react'
+
+const ListTodos = () => {
+
+    const [todos, setTodos] = useState([])
+
+    const getTodos = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/todos")
+            const jsonData = await response.json()
+            setTodos(jsonData)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    useEffect(() => {
+        getTodos()
+    }, [])
+
+    console.log(todos)
+    return (
+        <Fragment>
+            <table className="table mt-5 text-center">
+                <thead>
+                <tr>
+                    <th>description</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+                </thead>
+                <tbody>
+                {/*<tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>john@example.com</td>
+                </tr>*/}
+                {todos.map(todo => (
+                    <tr>
+                        <td>{todo.description}</td>
+                        <td>Edit</td>
+                        <td>Delete</td>
+                    </tr>
+                ))}
+                
+                </tbody>
+            </table>
+        </Fragment>
+    )
+}
+
+export default ListTodos
