100599da5a4cc567092fe0ae4a91075554d04503
Yoann <yoannpascalbouix@gmail.com>
2021-03-08 13:29:23 +0100

    Create todo

--- server/index.js
+++ server/index.js
@@ -11,11 +11,22 @@ app.use(express.json())
 
 //create a todo
 
-app.post("/todos", async(req,res) => {
-    //await
-})
+app.post("/todos", async (req, res) => {
+    try {
+      const { description } = req.body;
+      const newTodo = await pool.query(
+        "INSERT INTO todo (description) VALUES($1) RETURNING *",
+        [description]
+      );
+  
+      res.json(newTodo.rows[0]);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
 
 //get all todos
+
 
 //update a todo
 
